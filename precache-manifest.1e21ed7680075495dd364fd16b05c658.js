98ea6c2c4348fba3b9d32945bb24897179b17132
Jose de Lima <delimajm@gmail.com>
2019-10-20 02:24:35 -0500

    Updates

---
+++ precache-manifest.1e21ed7680075495dd364fd16b05c658.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "200b26f850515281cc451eee9c305d98",
+    "url": "/GRA_Reunion/index.html"
+  },
+  {
+    "revision": "7deec8cb5fd07f471ff7",
+    "url": "/GRA_Reunion/static/css/main.70db96d6.chunk.css"
+  },
+  {
+    "revision": "a5f7a4c8ac75d2318988",
+    "url": "/GRA_Reunion/static/js/2.77efc053.chunk.js"
+  },
+  {
+    "revision": "7deec8cb5fd07f471ff7",
+    "url": "/GRA_Reunion/static/js/main.52666e98.chunk.js"
+  },
+  {
+    "revision": "09b4fa26bdc227025b5f",
+    "url": "/GRA_Reunion/static/js/runtime~main.17dd90c7.js"
+  },
+  {
+    "revision": "9d5387a5e4d075d033a403428965aa13",
+    "url": "/GRA_Reunion/static/media/atlanta-sky-line.9d5387a5.jpg"
+  },
+  {
+    "revision": "bfc688ca490f0079051aea5b8ec75cbe",
+    "url": "/GRA_Reunion/static/media/ayo-ogunseinde-2.bfc688ca.jpg"
+  },
+  {
+    "revision": "bb4b9ac9fd07e48f86c719495b65341c",
+    "url": "/GRA_Reunion/static/media/clem-onojeghuo-2.bb4b9ac9.jpg"
+  },
+  {
+    "revision": "ae11d928aae96245020bb59a1910f758",
+    "url": "/GRA_Reunion/static/media/clem-onojeghuo-3.ae11d928.jpg"
+  },
+  {
+    "revision": "facbbdb787d77f975904ba9fa99b15c6",
+    "url": "/GRA_Reunion/static/media/daniel-olahh.facbbdb7.jpg"
+  },
+  {
+    "revision": "2051e0e847124c536df9c01a3f672032",
+    "url": "/GRA_Reunion/static/media/erik-lucatero-2.2051e0e8.jpg"
+  },
+  {
+    "revision": "f26c34bb88eb83d5aae99c7dbf3ddd48",
+    "url": "/GRA_Reunion/static/media/fabio-mangione.f26c34bb.jpg"
+  },
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/GRA_Reunion/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "be9fa468a029a195601370492c15519d",
+    "url": "/GRA_Reunion/static/media/login-image.be9fa468.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/GRA_Reunion/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/GRA_Reunion/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/GRA_Reunion/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/GRA_Reunion/static/media/nucleo-icons.f32ed5ad.eot"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.362a535cd40e5fa3a61bf2ca316a98e7.js"
+  "/GRA_Reunion/precache-manifest.1e21ed7680075495dd364fd16b05c658.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GRA_Reunion/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
