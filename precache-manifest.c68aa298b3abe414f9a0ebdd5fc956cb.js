13d41a2bf5a130d533d339d7f75974c2ff7bc402
Jose de Lima <delimajm@gmail.com>
2019-10-20 01:54:41 -0500

    Updates

---
+++ precache-manifest.c68aa298b3abe414f9a0ebdd5fc956cb.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95b1cf13d35a1dd11dd9b0acd9f7c69f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "022e60bf462414700314",
+    "url": "/static/css/main.df0eb7a8.chunk.css"
+  },
+  {
+    "revision": "a5f7a4c8ac75d2318988",
+    "url": "/static/js/2.77efc053.chunk.js"
+  },
+  {
+    "revision": "022e60bf462414700314",
+    "url": "/static/js/main.2e6a146a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9d5387a5e4d075d033a403428965aa13",
+    "url": "/static/media/atlanta-sky-line.9d5387a5.jpg"
+  },
+  {
+    "revision": "bfc688ca490f0079051aea5b8ec75cbe",
+    "url": "/static/media/ayo-ogunseinde-2.bfc688ca.jpg"
+  },
+  {
+    "revision": "bb4b9ac9fd07e48f86c719495b65341c",
+    "url": "/static/media/clem-onojeghuo-2.bb4b9ac9.jpg"
+  },
+  {
+    "revision": "ae11d928aae96245020bb59a1910f758",
+    "url": "/static/media/clem-onojeghuo-3.ae11d928.jpg"
+  },
+  {
+    "revision": "facbbdb787d77f975904ba9fa99b15c6",
+    "url": "/static/media/daniel-olahh.facbbdb7.jpg"
+  },
+  {
+    "revision": "2051e0e847124c536df9c01a3f672032",
+    "url": "/static/media/erik-lucatero-2.2051e0e8.jpg"
+  },
+  {
+    "revision": "f26c34bb88eb83d5aae99c7dbf3ddd48",
+    "url": "/static/media/fabio-mangione.f26c34bb.jpg"
+  },
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "be9fa468a029a195601370492c15519d",
+    "url": "/static/media/login-image.be9fa468.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/static/media/nucleo-icons.f32ed5ad.eot"
+  }
+]);
